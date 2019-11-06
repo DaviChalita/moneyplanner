529c557164f4cf9e2b940d7bc60ee12f64b3da97
DaviChalita <chalitameneguelli14@gmail.com>
2019-11-06 18:20:17 +0900

    read, write and update data test

--- app/src/main/java/com/dankook/moneyplanner/MainActivity.java
+++ app/src/main/java/com/dankook/moneyplanner/MainActivity.java
@@ -1,46 +1,64 @@
 package com.dankook.moneyplanner;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.dankook.moneyplanner.model.User;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     public static final int RC_SIGN_IN = 1;
-    private TextView txtViewUser;
+    private TextView txtWelcome;
+    private EditText txtBalance;
+    Button btBalance;
+    private FirebaseUser user;
+    User userModel;
+    private DatabaseReference mDatabase;
+    String email, name, balance, id;
+
     List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-            new AuthUI.IdpConfig.GoogleBuilder().build()
+            new AuthUI.IdpConfig.EmailBuilder().build()
     );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtWelcome = findViewById(R.id.txtViewUser);
+        txtBalance = findViewById(R.id.editText);
+        btBalance = findViewById(R.id.button);
         mFirebaseAuth = FirebaseAuth.getInstance();
-        txtViewUser = findViewById(R.id.txtViewUser);
+        mDatabase = FirebaseDatabase.getInstance().getReference("user");
         mAuthListener = new FirebaseAuth.AuthStateListener() {
+
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    String name = user.getDisplayName();
-                    txtViewUser.setText("Hello " + name);
+                    Toast.makeText(MainActivity.this, "User signed in", Toast.LENGTH_SHORT).show();
+
                 } else {
                     startActivityForResult(
                             AuthUI.getInstance()
@@ -52,11 +70,46 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        btBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addUser();
+            }
+        });
     }
 
-    public void clickEditAccount(View view) {
-        Intent goToEditAccount = new Intent(this, EditAccountActivity.class);
-        this.startActivity(goToEditAccount);
+    private void addUser() {
+        email = user.getEmail();
+        name = user.getDisplayName();
+        balance = txtBalance.getText().toString().trim();
+        id = mDatabase.push().getKey();
+
+        userModel = new User(id, email, name, balance);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User userSnapshotValue = userSnapshot.getValue(User.class);
+                    if (userSnapshotValue.getEmail().equals(email)) {
+                        System.out.println(userSnapshotValue.getEmail());
+                        System.out.println(email);
+                        System.out.println(userSnapshotValue.getId());
+                        mDatabase
+                                .child(userSnapshotValue.getId())
+                                .child("balance")
+                                .setValue(txtBalance.getText().toString());
+
+                        return;
+                    }
+                }
+                mDatabase.child(id).setValue(userModel);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void logout(View view) {
@@ -81,5 +134,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mFirebaseAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User userModel = userSnapshot.getValue(User.class);
+                    if (userModel.getEmail().equals(user.getEmail())) {
+                        txtWelcome.setText(userModel.getBalance());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
