b4f827d4ac02f1131955111070f8f13de24bda54
DaviChalita <chalitameneguelli14@gmail.com>
2019-11-30 23:06:19 +0900

    att 30/11

--- app/src/main/java/com/dankook/moneyplanner/CashWithdrawActivity.java
+++ app/src/main/java/com/dankook/moneyplanner/CashWithdrawActivity.java
@@ -1,6 +1,6 @@
 package com.dankook.moneyplanner;
 
-import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,20 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
-import android.widget.Toast;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.IdRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dankook.moneyplanner.model.Account;
+import com.dankook.moneyplanner.model.Alarm;
 import com.dankook.moneyplanner.model.User;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.common.internal.BaseGmsClient;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class CashWithdrawActivity extends AppCompatActivity {
 
@@ -29,22 +28,29 @@ public class CashWithdrawActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     User userModel;
     Account accountModel;
+    Alarm alarmModel;
     float newBalance, newSpend;
     EditText txtWithdraw;
     private RadioGroup radioGroup;
+    RadioButton foodBtn, shopBtn, leisBtn, transpBtn, etcBtn;
+    String selectedRadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash_withdraw);
 
-        Intent myintent2 = getIntent();
-
         ImageButton btnBack = (ImageButton) findViewById(R.id.btnBack);
         Button btncardwithdrawok = (Button) findViewById(R.id.btn_cardwithdraw_ok);
 
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
+        txtWithdraw = findViewById(R.id.Cash_Withdraw);
+        foodBtn = findViewById(R.id.imgbtnfood);
+        shopBtn = findViewById(R.id.imgbtnshopping);
+        leisBtn = findViewById(R.id.imgbtnleisure);
+        transpBtn = findViewById(R.id.imgbtntransport);
+        etcBtn = findViewById(R.id.imgbtnetc);
 
         mDatabase = FirebaseDatabase.getInstance().getReference("user");
 
@@ -52,10 +58,8 @@ public class CashWithdrawActivity extends AppCompatActivity {
         Bundle extras = intent.getExtras();
         userModel = (User) extras.getSerializable("user");
         accountModel = (Account) extras.getSerializable("account");
-        System.out.println("foi pra tela de withdraw");
-        System.out.println(userModel.getEmail());
-        System.out.println(accountModel.getBalance());
-
+        alarmModel = (Alarm) extras.getSerializable("alarm");
+        System.out.println("Alarm "+alarmModel.getLimit());
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,75 +71,78 @@ public class CashWithdrawActivity extends AppCompatActivity {
             }
         });
 
+
         btncardwithdrawok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.imgbtnfood:
+                        selectedRadBtn = "food";
+                        break;
+                    case R.id.imgbtnshopping:
+                        selectedRadBtn = "shopping";
+                        break;
+                    case R.id.imgbtnleisure:
+                        selectedRadBtn = "leisure";
+                        break;
+                    case R.id.imgbtntransport:
+                        selectedRadBtn = "transport";
+                        break;
+                    case R.id.imgbtnetc:
+                        selectedRadBtn = "etc";
+                        break;
+                    default:
+                        selectedRadBtn = "unselected";
+                        break;
+                }
 
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                System.out.println(userModel.getEmail());
                 name = userModel.getName();
                 withdraw = txtWithdraw.getText().toString().trim();
-                newBalance = accountModel.getBalance() - Float.parseFloat(withdraw);
-                System.out.println("))))))))))))))))))))))))))))))))))))))");
-                newSpend = accountModel.getSpend()+Float.parseFloat(withdraw);
-                newSpendTxt = Float.toString(newSpend);
-                System.out.println(newSpend);
-                accountModel.setSpend(newSpendTxt);
-                newBalancetxt = Float.toString(newBalance);
                 id = accountModel.getId();
-                accountModel.setBalance(Float.toString(newBalance));
-                System.out.println(accountModel.getBalance());
-
-                mDatabase.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                            User userSnapshotValue = userSnapshot.getValue(User.class);
-                            if (userSnapshotValue.getEmail().equals(userModel.getEmail())) {
-                                System.out.println(userSnapshotValue.getEmail());
-                                System.out.println(email);
-                                System.out.println(userSnapshotValue.getId());
-                                mDatabase
-                                        .child(userSnapshotValue.getId())
-                                        .child("balance")
-                                        .setValue(newBalancetxt);
-                                mDatabase.child(userSnapshotValue.getId()).child("spend").setValue(newSpendTxt);
-                                return;
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                try {
+                    if (alarmModel.getLimit() > (accountModel.getSpend() + Float.parseFloat(withdraw))) {
+                        newBalance = accountModel.getBalance() - Float.parseFloat(withdraw);
+                        newSpend = accountModel.getSpend() + Float.parseFloat(withdraw);
+                        newSpendTxt = Float.toString(newSpend);
+                        accountModel.setSpend(newSpendTxt);
+                        newBalancetxt = Float.toString(newBalance);
+                        accountModel.setBalance(Float.toString(newBalance));
+                        mDatabase
+                                .child(id)
+                                .child("balance")
+                                .setValue(newBalancetxt);
+                        mDatabase.child(id).child("spend").setValue(newSpendTxt);
+                        finish();
+                    } else {
+                        new AlertDialog.Builder(CashWithdrawActivity.this).setTitle("Not enough money")
+                                .setMessage("The money spent surpass the limit setted!")
+                                .setPositiveButton("I understand", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                    }
+                                }).show();
 
                     }
-                });
 
-                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
-                Intent myintent = new Intent(CashWithdrawActivity.this, MainActivity.class);
-                startActivity(myintent);
-                finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
-
     }
 
     RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-            if(i == R.id.imgbtnetc){
+            if (i == R.id.imgbtnetc) {
                 Toast.makeText(CashWithdrawActivity.this, "clicked ect", Toast.LENGTH_SHORT).show();
-            }
-            else if(i == R.id.imgbtnfood){
+            } else if (i == R.id.imgbtnfood) {
                 Toast.makeText(CashWithdrawActivity.this, "clicked food", Toast.LENGTH_SHORT).show();
-            }
-            else if(i == R.id.imgbtnleisure){
+            } else if (i == R.id.imgbtnleisure) {
                 Toast.makeText(CashWithdrawActivity.this, "clicked leisure", Toast.LENGTH_SHORT).show();
-            }
-            else if(i == R.id.imgbtnshopping){
+            } else if (i == R.id.imgbtnshopping) {
                 Toast.makeText(CashWithdrawActivity.this, "clicked shopping", Toast.LENGTH_SHORT).show();
-            }
-            else if(i == R.id.imgbtntransport){
+            } else if (i == R.id.imgbtntransport) {
                 Toast.makeText(CashWithdrawActivity.this, "clicked transportation", Toast.LENGTH_SHORT).show();
             }
 
@@ -143,8 +150,4 @@ public class CashWithdrawActivity extends AppCompatActivity {
     };
 
 
-
 }
-
-
-
--- app/src/main/java/com/dankook/moneyplanner/MainActivity.java
+++ app/src/main/java/com/dankook/moneyplanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dankook.moneyplanner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +11,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dankook.moneyplanner.model.Account;
+import com.dankook.moneyplanner.model.Alarm;
 import com.dankook.moneyplanner.model.User;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     float newBalance;
     Account accountModel;
     Switch switchAlarm;
+    Alarm alarmModel;
 
     List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build()
@@ -56,11 +60,6 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         txtWelcome = findViewById(R.id.txtViewUser);
         switchAlarm = findViewById(R.id.Alarm_switch);
         txtCash = findViewById(R.id.Cash_Account);
-        /*txtBalance = findViewById(R.id.editTextBalance);
-        btBalance = findViewById(R.id.buttonBalance);
-        txtSpends = findViewById(R.id.editTextSpends);
-        btSpends = findViewById(R.id.buttonSpends); */
-
         Button Cash_Deposit = (Button) findViewById(R.id.Cash_Deposit);
         Button Cash_Withdraw = (Button) findViewById(R.id.Cash_Withdraw);
 
@@ -87,79 +86,15 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 }
             }
         };
-
-       /* mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (user != null) {
-                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                        User userModel = userSnapshot.getValue(User.class);
-                        if (userModel.getEmail().equals(user.getEmail())) {
-                            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                            System.out.println(userModel.getId());
-                            System.out.println(userModel.getEmail());
-                            System.out.println(userModel.getName());
-                            accountModel = userSnapshot.getValue(Account.class);
-                            System.out.println(accountModel.getId());
-                            System.out.println(accountModel.getBalance());
-
-                            txtWelcome.setText(userModel.getName() + "'s");
-                            txtCash.setText(Float.toString(accountModel.getBalance()));
-                            return;
-                        }
-                    }
-
-                }
-            }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });*/
-
-        //  addUser();
-
-     /*   Cash_Withdraw.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Cash Withdraw page", Toast.LENGTH_LONG).show();
-                Intent myintent = new Intent(MainActivity.this, CashWithdrawActivity.class);
-                startActivity(myintent);
-            }
-        });*/
-
-        /*btBalance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addUser();
-            }
-        });
-        btSpends.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addSpends();
-            }
-        });*/
-
     }
 
 
     public void clickDeposit(View view) {
         Toast.makeText(getApplicationContext(), "Cash Deposit page", Toast.LENGTH_LONG).show();
-        //addExtra, save the user in firebase with balance = 0 if null
         Intent myIntent = new Intent(MainActivity.this, CashDepositActivity.class);
-        System.out.println("###############################");
-        System.out.println(userModel.getEmail());
-        System.out.println(userModel.getName());
-        System.out.println(accountModel.getBalance());
-        //System.out.println(accountModel.getBalance());
         Bundle extras = new Bundle();
         extras.putSerializable("user", userModel);
         extras.putSerializable("account", accountModel);
-        //myIntent.putExtra("user", userModel);
-        //myIntent.putExtra("account", accountModel);
         myIntent.putExtras(extras);
         startActivity(myIntent);
 
@@ -167,18 +102,11 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 
     public void clickWithdraw(View view) {
         Toast.makeText(getApplicationContext(), "Cash Withdraw page", Toast.LENGTH_LONG).show();
-        //addExtra, save the user in firebase with balance = 0 if null
         Intent myIntent = new Intent(MainActivity.this, CashWithdrawActivity.class);
-        System.out.println("$$$$$$$$$$$$$$$$");
-        System.out.println(userModel);
-        System.out.println(userModel.getEmail());
-        System.out.println(userModel.getName());
-        System.out.println(accountModel.getBalance());
         Bundle extras = new Bundle();
         extras.putSerializable("user", userModel);
         extras.putSerializable("account", accountModel);
-        myIntent.putExtra("user", userModel);
-        myIntent.putExtra("account", accountModel);
+        extras.putSerializable("alarm", alarmModel);
         myIntent.putExtras(extras);
         startActivity(myIntent);
     }
@@ -231,8 +159,10 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onResume() {
         super.onResume();
+        userModel = null;
+        accountModel = null;
+        alarmModel = null;
         mFirebaseAuth.addAuthStateListener(mAuthListener);
-        //txtCash.setText(Float.toString(accountModel.getBalance()));
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -240,16 +170,15 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                     for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                         User userModel = userSnapshot.getValue(User.class);
                         if (userModel.getEmail().equals(user.getEmail())) {
-                            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                            System.out.println(userModel.getId());
-                            System.out.println(userModel.getEmail());
-                            System.out.println(userModel.getName());
                             accountModel = userSnapshot.getValue(Account.class);
-                            System.out.println(accountModel.getId());
-                            System.out.println(accountModel.getBalance());
-                            System.out.println(accountModel.getSpend());
                             txtWelcome.setText(userModel.getName() + "'s");
                             txtCash.setText(Float.toString(accountModel.getBalance()));
+                            try {
+                                alarmModel = userSnapshot.getValue(Alarm.class);
+                                switchAlarm.setChecked(true);
+                            } catch (Exception e) {
+                                switchAlarm.setChecked(false);
+                            }
                             return;
                         }
                     }
@@ -274,37 +203,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onStart() {
         super.onStart();
-        /*mFirebaseAuth = FirebaseAuth.getInstance();
-        final FirebaseUser userStart = FirebaseAuth.getInstance().getCurrentUser();
-        System.out.println("#################################################");
-        System.out.println(userStart.getEmail());
-        if (userStart != null) {
-            mDatabase.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                        User userModel = userSnapshot.getValue(User.class);
-                        accountModel = userSnapshot.getValue(Account.class);
-                        if (userModel.getEmail().equals(userStart.getEmail())) {
-
-                            txtWelcome.setText(userModel.getName() + "'s Total Account");
-
-                            txtCash.setText(Float.toString(accountModel.getBalance()));
-                        }
-                    }
-                }
-
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
-        } else {
-            //dont retrieve the balance
-        }*/
-    }
+      }
 
 
     public void clickCategories(View view) {
@@ -321,11 +220,33 @@ public class MainActivity extends AppCompatActivity implements Serializable {
 
     public void clickAlarmSwitch(View view) {
         if (switchAlarm.isChecked()) {
-            Intent intent = new Intent(this, AlarmActivity.class);
-            startActivity(intent);
+            Intent myIntent = new Intent(MainActivity.this, AlarmActivity.class);
+            Bundle extras = new Bundle();
+            System.out.println("Indo para tela alarme");
+            System.out.println(userModel.getEmail());
+            System.out.println(accountModel.getBalance());
+            extras.putSerializable("user", userModel);
+            extras.putSerializable("account", accountModel);
+            myIntent.putExtra("user", userModel);
+            myIntent.putExtra("account", accountModel);
+            myIntent.putExtras(extras);
+            startActivity(myIntent);
+        } else {
+
+            new AlertDialog.Builder(MainActivity.this).setTitle("Alarm off")
+                    .setMessage("Do you want to turn the alarm off?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            switchAlarm.setChecked(false);
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    switchAlarm.setChecked(true);
+                }
+            }).show();
         }
-
     }
-
 }
 
